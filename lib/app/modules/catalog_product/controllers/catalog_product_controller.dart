92bc9f5ad0ac47c80ef5a7118ad194070b96b095
Sam_76 <muhammadusamah76@gmail.com>
2024-06-07 22:58:46 +0700

    update catalog view with dialog

--- lib/app/modules/catalog_product/controllers/catalog_product_controller.dart
+++ lib/app/modules/catalog_product/controllers/catalog_product_controller.dart
@@ -1,12 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CatalogProductController extends GetxController {
-  //TODO: Implement CatalogProductController
+  late TextEditingController updateProductNameController;
+  late TextEditingController updateProductStock;
+  late TextEditingController updateProductCategory;
+  late TextEditingController updateProductPrice;
 
   final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    updateProductNameController = TextEditingController();
+    updateProductStock = TextEditingController();
+    updateProductCategory = TextEditingController();
+    updateProductPrice = TextEditingController();
   }
 
   @override
--- lib/app/modules/catalog_product/views/catalog_product_view.dart
+++ lib/app/modules/catalog_product/views/catalog_product_view.dart
@@ -1,3 +1,4 @@
+import 'package:bionic/app/components/custom_add_product_dialog.dart';
 import 'package:bionic/app/components/custom_catalog_item.dart';
 import 'package:bionic/app/utils/utility.dart';
 import 'package:flutter/material.dart';
@@ -11,49 +12,53 @@ class CatalogProductView extends GetView<CatalogProductController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Row(
-          children: [
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color.fromRGBO(140, 140, 140, 1),
-                    ),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          SliverAppBar(
+            floating: true,
+            snap: true,
+            backgroundColor: Colors.white,
+            leading: GestureDetector(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: paddingSmall,
+                  top: paddingVerySmall,
+                  bottom: paddingVerySmall,
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(242, 242, 242, 1),
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
-                  contentPadding: EdgeInsets.all(1),
-                  border: OutlineInputBorder(),
-                  hintText: 'search',
-                  prefixIcon: Icon(Icons.search),
+                  child: const Icon(Icons.arrow_back_rounded),
                 ),
               ),
+              onTap: () {
+                Get.back();
+              },
             ),
-          ],
-        ),
-        leading: GestureDetector(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: paddingSmall,
-              top: paddingVerySmall,
-              bottom: paddingVerySmall,
-            ),
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color.fromRGBO(242, 242, 242, 1),
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: const Icon(Icons.arrow_back_rounded),
+            title: const Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(140, 140, 140, 1),
+                        ),
+                      ),
+                      contentPadding: EdgeInsets.all(1),
+                      border: OutlineInputBorder(),
+                      hintText: 'search',
+                      prefixIcon: Icon(Icons.search),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          onTap: () {
-            Get.back();
-          },
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: paddingLarge),
-        child: ListView.builder(
+        ],
+        body: ListView.builder(
           itemCount: 5,
           itemBuilder: (context, index) {
             return Padding(
@@ -63,6 +68,23 @@ class CatalogProductView extends GetView<CatalogProductController> {
               ),
               child: GestureDetector(
                 child: const CustomCatalogItem(),
+                onTap: () {
+                  Get.dialog(
+                    CustomAddProductDialog(
+                      productName: controller.updateProductNameController,
+                      productCategory: controller.updateProductCategory,
+                      productPrice: controller.updateProductPrice,
+                      productStock: controller.updateProductStock,
+                      onTap: () {
+                        Get.back();
+                        Get.snackbar(
+                          controller.updateProductNameController.text,
+                          'message',
+                        );
+                      },
+                    ),
+                  );
+                },
               ),
             );
           },
