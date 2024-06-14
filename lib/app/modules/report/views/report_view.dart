8a87f412e883e91fbde5fe10ad716b56753271a6
Ahmad Alan Lestari <paharikoki@gmail.com>
2024-06-14 17:09:54 +0700

    feature sales view

---
+++ lib/app/modules/report/views/report_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/report_controller.dart';
+
+class ReportView extends GetView<ReportController> {
+  const ReportView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Laporan Penjualan'),
+        centerTitle: true,
+        
+      ),
+      body: const Center(
+        child: Text(
+          'ReportView is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
