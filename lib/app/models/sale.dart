503d4b8d172c9e6056582bd3a1d102e7cc656068
Sam_76 <muhammadusamah76@gmail.com>
2024-06-21 21:17:33 +0700

    log pembelian

---
+++ lib/app/models/sale.dart
@@ -0,0 +1,86 @@
+class Sale {
+  String storeId;
+  String total;
+  String address;
+  String discount;
+  String name;
+  String phone;
+  List<Products> products;
+
+  Sale({
+    required this.storeId,
+    required this.total,
+    required this.address,
+    required this.discount,
+    required this.name,
+    required this.phone,
+    required this.products,
+  });
+
+  factory Sale.fromJson(Map<String, dynamic> json) {
+    List<dynamic> productsJson = json['products'];
+    List<Products> products = productsJson.map((productJson) {
+      return Products.fromJson(productJson);
+    }).toList();
+
+    return Sale(
+      storeId: json["store_id"],
+      total: json["total"],
+      address: json["address"],
+      discount: json["discount"],
+      name: json["name"],
+      phone: json["phone"],
+      products: products,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        "store_id": storeId,
+        "total": total,
+        "address": address,
+        "discount": discount,
+        "name": name,
+        "phone": phone,
+        "products": products.map((product) => product.toJson()).toList(),
+      };
+}
+
+class Products {
+  String category;
+  String id;
+  String image;
+  String name;
+  int price;
+  int qty;
+  int stock;
+
+  Products({
+    required this.category,
+    required this.id,
+    required this.image,
+    required this.name,
+    required this.price,
+    required this.qty,
+    required this.stock,
+  });
+
+  factory Products.fromJson(Map<String, dynamic> json) => Products(
+        category: json["category"],
+        id: json["id"],
+        image: json["image"],
+        name: json["name"],
+        price: json["price"],
+        qty: json["qty"],
+        stock: json["stock"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "category": category,
+        "id": id,
+        "image": image,
+        "name": name,
+        "price": price,
+        "qty": qty,
+        "stock": stock,
+      };
+}
